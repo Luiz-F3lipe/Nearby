7729cb5680115f848eff2bc6ccd1062a86a50003
Luiz-F3lipe <luizf3lipe.dev@gmail.com>
2024-12-09 13:24:56 -0300

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Luiz Felipe Moraes Lima on 09/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
